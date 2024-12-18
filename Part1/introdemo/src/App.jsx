ecd0e78594056ecf870acf377fc46d3d38e8d6bc
Devin <valdezdevin@gmail.com>
2024-12-18 04:26:25 -0800

    part 1 thru props, i've added a line to eslint config file to silence the react/prop-types warning

--- Part1/introdemo/src/App.jsx
+++ Part1/introdemo/src/App.jsx
@@ -1,17 +1,26 @@
-const App = () => {
-
-  const now = new Date()
-  const a = 10
-  const b = 20
-  console.log(now, a+b)
+const Hello = (props) => {
+  console.log(props)  
   return (
     <div>
-      <p>Hello world, it is {now.toString()}</p>
       <p>
-        {a} plus {b} is {a + b}
+        Hello {props.name}, you are {props.age} years old      
       </p>
     </div>
   )
 }
 
+const App = () => {
+  const name = 'Peter'  
+  const age = 10
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name='Maya' age={26 + age} />      
+      <Hello name={name} age={age} />    
+    </div>
+  )
+}
+
+
+// remember to not remove this
 export default App

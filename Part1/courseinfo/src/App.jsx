3eb5f3970fabc433f9eaf36d44a42420d779487c
Devin <valdezdevin@gmail.com>
2024-12-28 00:07:50 -0800

    in progress of assignment 1.1, just created the content component and trying to understand how .map works

--- Part1/courseinfo/src/App.jsx
+++ Part1/courseinfo/src/App.jsx
@@ -1,5 +1,29 @@
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part, index) => (
+        <p key={index}>{part.name} contains {part.exercises} exercises</p>
+      ))}
+    </div>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
+  const parts = [
+    { name: 'Fundamentals of React', exercises: 10 },
+    { name: 'Using props to pass data', exercises: 7 },
+    { name: 'State of a component', exercises: 14 }
+  ]
+  
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
   const part2 = 'Using props to pass data'
@@ -9,16 +33,8 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Header course={course} />
+      <Content parts={parts} />
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )

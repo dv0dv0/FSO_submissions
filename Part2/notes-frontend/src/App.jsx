2d1dfd70a4d1a86e0a19dd30ece36a3bbb9ddcf7
Devin <valdezdevin@gmail.com>
2025-01-20 22:17:20 -0800

    got notes adding halfway thru forms page

--- Part2/notes-frontend/src/App.jsx
+++ Part2/notes-frontend/src/App.jsx
@@ -4,6 +4,28 @@ import Note from './components/Note'
 
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState(
+    'a new note...'
+  )
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  const addNote = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+    
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: String(notes.length + 1),
+    }
+  
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
 
   return (
     <div>
@@ -13,6 +35,14 @@ const App = (props) => {
           <Note key={note.id} note={note} />
         )}
       </ul>
+
+      <form onSubmit={addNote}>
+        <input 
+          value={newNote}
+          onChange={handleNoteChange} 
+        />
+        <button type="submit">save</button>
+      </form>   
     </div>
   )
 }
